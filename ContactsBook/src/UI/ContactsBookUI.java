0338c10fb9d04e2a4d1f49229c857e00932e2f90
IvanRDelgado <irdelgadog@unal.edu.co>
2016-10-26 00:00:07 -0500

    Alpha 57
    Actualizacion metodos UI

--- ContactsBook/src/UI/ContactsBookUI.java
+++ ContactsBook/src/UI/ContactsBookUI.java
@@ -4,37 +4,66 @@
  * and open the template in the editor.
  */
 package UI;
-import logic .*;
-import Data .*;
+
+import logic.*;
+import Data.*;
 import java.util.Scanner;
+
 /**
  *
  * @author johanr98 jsromerod@unal.edu.co
  */
 public class ContactsBookUI {
-    public static String ingreso_String(){
+
+    public static String ingreso_String() {
         String entradaTeclado = "";
-        Scanner entradaEscaner = new Scanner (System.in);
-        entradaTeclado = entradaEscaner.nextLine ();
+        Scanner entradaEscaner = new Scanner(System.in);
+        entradaTeclado = entradaEscaner.nextLine();
         return entradaTeclado;
     }
-    public static int ingreso_int(){
+
+    public static int ingresoInt() {
         int entradaTeclado;
-        Scanner entradaEscaner = new Scanner (System.in);
+        Scanner entradaEscaner = new Scanner(System.in);
         entradaTeclado = entradaEscaner.nextInt();
         return entradaTeclado;
     }
-    
-    public static void printString(String a){
+
+    public static long ingresoLong() {
+        long entradaTeclado;
+        Scanner entradaEscaner = new Scanner(System.in);
+        entradaTeclado = entradaEscaner.nextLong();
+        return entradaTeclado;
+    }
+
+    public static void printString(String a) {
         System.out.println(a);
     }
-    
-    public static void printContact(Contact a){
+
+    public static void printContact(Contact a) {
         System.out.println(a);
     }
-     
-    public static void printInfoAddConrtact (){
-        System.out.println("Adicione el nuevo contacto: ");        
+
+    public static void printInfoAddContact() {
+        System.out.println("Adicione el nuevo contacto: ");
+    }
+
+    public static void printWelcome() {
+        System.out.println(" LIBRETA DE CONTACTOS ");
+        System.out.println(" LIBRETA DE CONTACTOS ");
+    }
+    public static void printMethodOptions() {
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("[1]- Adicionar contacto");
+        System.out.println("[2]- Eliminar contacto");
+        System.out.println("[3]- Actualizar contacto");
+        System.out.println("[4]- Visualizar todos los contactos");
+        System.out.println("[5]- Visualizar un Contacto");
+        System.out.println("[6]- Salir");
+        
+        
+        
     }
     
+
 }
