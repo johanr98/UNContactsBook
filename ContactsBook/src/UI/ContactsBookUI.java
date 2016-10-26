54f54e2a3e546f42725a8f0aeeeec8387434b803
IvanRDelgado <irdelgadog@unal.edu.co>
2016-10-25 23:37:01 -0500

    Alpha 2222222
    UI Method Contact

--- ContactsBook/src/UI/ContactsBookUI.java
+++ ContactsBook/src/UI/ContactsBookUI.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package UI;
-
+import logic .*;
+import Data .*;
+import java.util.Scanner;
 /**
  *
  * @author johanr98 jsromerod@unal.edu.co
  */
 public class ContactsBookUI {
+    public static String ingreso_String(){
+        String entradaTeclado = "";
+        Scanner entradaEscaner = new Scanner (System.in);
+        entradaTeclado = entradaEscaner.nextLine ();
+        return entradaTeclado;
+    }
+    public static int ingreso_int(){
+        int entradaTeclado;
+        Scanner entradaEscaner = new Scanner (System.in);
+        entradaTeclado = entradaEscaner.nextInt();
+        return entradaTeclado;
+    }
+    
+    public static void printString(String a){
+        System.out.println(a);
+    }
+    
+    public static void printContact(Contact a){
+        System.out.println(a);
+    }
+     
+    public static void printInfoAddConrtact (){
+        System.out.println("Adicione el nuevo contacto: ");        
+    }
     
 }
