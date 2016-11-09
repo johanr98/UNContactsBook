10bc593ad4f8e0870cdf78f864536f19dc265b40
IvanRDelgado <irdelgadog@unal.edu.co>
2016-11-08 20:28:03 -0500

    ya funciona

--- ContactsBook/src/UI/ContactsBookGUI.java
+++ ContactsBook/src/UI/ContactsBookGUI.java
@@ -230,7 +230,7 @@ public class ContactsBookGUI extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -259,10 +259,9 @@ public class ContactsBookGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-<<<<<<< HEAD
+
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-=======
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -270,6 +269,5 @@ public class ContactsBookGUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JPanel jPanel1;
->>>>>>> origin/master
     // End of variables declaration//GEN-END:variables
 }
